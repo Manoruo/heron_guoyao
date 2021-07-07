91b0731bc5cf152625e1ac2a7773267dd4d4ac51
Manoruo <72631682+Manoruo@users.noreply.github.com>
2021-07-07 18:59:22 -0400

    Update waypoint_navigator.py
    Lawn mower function implemented (not fully working yet)

--- scripts/waypoint_navigator.py
+++ scripts/waypoint_navigator.py
@@ -15,27 +15,98 @@ pos_cur=NavSatFix()
 yaw_cur = 0.00
 yaw_des_old = 0.0
 
-latitude = 49.900000001193085
-longitude = 8.900000001091586
+latitude = 39.94339440042317
+longitude = -75.19981109992537
+
+# conversions from lon/lat to meters
+met_lat = 111033.4717 
+met_lon = 85467.2528
 
 square_leg_length = 0.0001
 
-course_desired = ([latitude, longitude],
-                  [latitude + square_leg_length, longitude],
-                  [latitude + square_leg_length, longitude + square_leg_length],
-                  [latitude, longitude + square_leg_length],
-                  [latitude, longitude])
-
-
+course = ([latitude, longitude], 
+          [latitude + square_leg_length, longitude], 
+          [latitude + square_leg_length, longitude + square_leg_length],
+          [latitude, longitude + square_leg_length],
+          [latitude, longitude])
+         
 i = 0
 kp=2
 wypt_dist_thresh = 2.0  # distance threshold to stop or switch to next way point
 cmd_dt = 0.1            # time interval for cmd_helm publishing
 base_thrust = 1.0       # the base thrust level at which the Heron will run
 
-# conversions from lon/lat to meters
-met_lat = 111033.4717 
-met_lon = 85467.2528
+
+flag = "v"
+
+
+def lawn_mower(course, flag="v"):
+    points = 7
+    flat_len = square_leg_length
+    
+    num_points = len(course)
+    new_course = []
+    
+    if num_points >= 4:
+         # Assuming we get a square entry
+
+        p1 = course[0]
+        p2 = course[1]
+        p4 = course[3]
+
+        width = (p2[0] - p1[0])
+        height = (p4[1] - p1[1])
+
+        print("Width, Height:", width, height)
+
+        if flag == "v":
+            flat = width * 0.5
+            
+            """for i in range(points):
+                delta_y = (math.cos(-math.pi * i) * height) + p1[1]
+                delta_x = (math.sin(-math.pi * i / 2) * flat_len) + p1[0]
+                new_course[i] = [delta_x, delta_y]
+            print(new_course)
+            """
+            # Hard Coded
+            new_course = [[latitude, longitude],
+                          [latitude, longitude + height],
+                          [latitude + flat, longitude + height],
+                          [latitude + flat, longitude],
+                          [latitude + (2 * flat), longitude],
+                          [latitude + (2 * flat), longitude + height],
+                          [latitude + (3 * flat), longitude + height],
+                          [latitude + (3 * flat), longitude]]
+            # Loop code 
+            x_multi = 0
+            y_multi = 0 
+            
+            for i in range(points):
+               
+                if i != 0 and (i % 3) != 0:
+                    lon = longitude + (height)
+                else:
+                    lon = longitude
+
+                lat = latitude + (x_multi * flat)
+                
+                new_course[i] = [lat, lon]
+                
+                if i % 2 == 1:
+                    x_multi += 1
+                
+            return new_course
+        elif flag == "h":
+            pass
+        
+        
+    else:
+        print("Supply at least 4 points")
+
+    return course
+
+# just for testing, take this out 
+course_desired = lawn_mower(course, flag)
 
 def nav_comp(navsat_msg):
     global pos_cur
@@ -61,12 +132,15 @@ def control_publisher(event):
     pos_des_lon = course_desired[i][1]
 
     delta_lat = (pos_des_lat-pos_cur.latitude)*met_lat
-    delta_lon = (pos_des_lon - pos_cur.longitude)*met_lon
+    delta_lon = (pos_des_lon - pos_cur.longitude)*met_lon    
     dist_err = math.sqrt( delta_lat**2 + delta_lon**2 )
+    
     print ("dist error", dist_err)
     print (i)
+
     lat_pub.publish(float(course_desired[i][0]))
     long_pub.publish(float(course_desired[i][1]))
+
     if (dist_err < wypt_dist_thresh) :
         # go to next way point if within distance threshold
         if( i < len(course_desired)-1 ):
